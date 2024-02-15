407a82ec0f8642240b71058298d0e9dd7808ade2
Samuel Raymond <samuelraymond711@gmail.com>
2024-02-15 16:23:50 +0700

    fix image

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
     <motion.nav variants={navVariants} initial="hidden" whileInView="show" className={`${styles.xPaddings} py-8 relative`}>
       <div className=' absolute w-[50%] inset-0 gradient-01'/>
       <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8 relative`}>
-        <img src="search.svg" alt="search" className=' w-[24px] h-[24px] object-contain'/>
+        <img src="./search.svg" alt="search" className=' w-[24px] h-[24px] object-contain'/>
         <h2 className=' font-extrabold text-[24px] leading-[30px] text-white'>METAVERSUS</h2>
-        <img src={`${toggle?"close.svg":"menu.svg"}`} alt="menu" className='w-[24px] h-[24px] object-contain cursor-pointer' onClick={()=>settoggle((prev)=>(!prev))} />
+        <img src={`${toggle?"./close.svg":"./menu.svg"}`} alt="menu" className='w-[24px] h-[24px] object-contain cursor-pointer' onClick={()=>settoggle((prev)=>(!prev))} />
 
         <div className={`${toggle?'flex':'hidden'} p-6 bg-bp-gradient absolute top-9 right-0 min-w-[140px] rounded-xl z-20 text-white`}>
           <ul className=' list-none flex items-end flex-1 flex-col'>
